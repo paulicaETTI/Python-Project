e0be4cad89814c3ca4d8ac9aeec84fd2da56a0fb
unknown <paul.vallasutean@ymail.com>
2023-12-11 21:28:27 +0200

    rock paper scissors

--- 04.exercitii/rock_paper_scissors.py
+++ 04.exercitii/rock_paper_scissors.py
@@ -1,33 +1,46 @@
 import random
+def outcome(player, computer):
+    if player == computer:
+        return 0  # Draw
+    elif (player == 'r' and computer == 's') or (player == 'p' and computer == 'r') or (player == 's' and computer == 'p'):
+        return 1  # Player wins
+    else:
+        return -1  # Computer wins
+
 def computer_choice():
     choice = random.randint(1, 3)
     if choice == 1:
-        return "r", "rock"
+        return 'r', 'rock'
     elif choice == 2:
-        return "p", "paper"
+        return 'p', 'paper'
     else:
-        return "s", "scissors"
+        return 's', 'scissors'
 
-print("Welcome to rock, paper, scissors!")
-print("To quit the game enter 'quit' or enter r/p/s to play VS the computer.\n")
+score_player = 0
+score_computer = 0
 
 while True:
-    score_user = 0
-    score_computer = 0
-    user_command = input("Enter command: ")
-    computer_symbol, computer_name = computer_choice()
-    if user_command == "quit":
+    player_command = input("Enter command: ")
+    computer_command, computer_name = computer_choice()
+
+    if player_command == "quit":
+        print(f"Final score is: Player-{score_player}, Computer-{score_computer}")
         break
-    elif user_command == 'r':
-        print(f"You selected rock!")
+    elif player_command in ['r', 'p', 's']:
+        print(f"You selected {player_command}!")
         print(f"Computer selected {computer_name}!")
-    elif user_command == 'p':
-        print(f"You selected paper!")
-        print(f"Computer selected {computer_name}!")
-    elif user_command == 'r':
-        print(f"You selected scissors!")
-        print(f"Computer selected {computer_name}!")
+
+        outcome_result = outcome(player_command, computer_command)
+
+        if outcome_result == 0:
+            print("It's a draw!")
+        elif outcome_result == 1:
+            print("You win!")
+            score_player += 1
+        else:
+            print("Computer wins!")
+            score_computer += 1
+
+        print(f"Score: Player: {score_player}, Computer: {score_computer}")
     else:
-        print("Please select a valid command('quit','r','p','s')")
-
-
+        print("Please select a valid command ('quit', 'r', 'p', 's')")
