ba8fe27cd3254da9ee10cc89f99fa1aaeb86bac2
unknown <paul.vallasutean@ymail.com>
2023-12-12 22:36:06 +0200

    sesiunea 5

---
+++ 05.web_scraping/exercitii_pandas.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+# dintr o lista
+
+lista = [10, 20, 30, 40, 50]
+etichete = ['a', 'b', 'c', 'd', 'e']
+serie = pd.Series(lista, index=etichete)
+print(serie)
+
+# dintr un numpy array
+
+# array_date = np.array([10,20,30,40,50])
+# serie = pd.Series(array_date)
+# print(serie)
+
+# dintr un dictionar
+# dict_date = {'a': 10, 'b': 20, 'c': 30, 'd': 40, 'e': 50}
+# serie = pd.Series(dict_date)
+# print(serie)
+
+
+
