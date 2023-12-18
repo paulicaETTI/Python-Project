a25c9bfad7038d8d5dc50c1f775fe0fb828f917d
Paul <paul@twm.ro>
2023-12-18 11:38:31 +0200

    Tema covid

--- 07.tema_scraping/01_covid.py
+++ 07.tema_scraping/01_covid.py
@@ -1,50 +1,92 @@
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import re
+import pandas as pd
 
 option = webdriver.ChromeOptions()
 option.add_argument('start-maximized')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
-driver.get('https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-10-decembrie-ora-13-00-2/')
 
 dictionar = {
-        'Nr. crt.': [],
-        'Judet': [],
-        '10.12.2021': []
-    }
+            'Nr. crt.': [],
+            'Judet': []
+        }
 
-try:
-    button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[6]/div/div[3]/button[1]")))
-    button.click()
-    time.sleep(2)
-finally:
-    table = driver.find_element(by=By.XPATH, value='//*[@id="post-28531"]/div/div/table[1]/tbody')
-    table_list = table.text.split('\n')
-    del table_list[0]
-    for row_index, row_value in enumerate(table_list):
-        if row_index < 42:
-            dictionar['Nr. crt.'].append(row_index + 1)
-            row_value_split = row_value.split(' ')
-            number = row_value_split[-2]
-            del row_value_split[0]
-            del row_value_split[-3:]
-            judet = str(row_value_split)
-            dictionar['Judet'].append(judet)
-            dictionar['10.12.2021'].append(number)
-    print(dictionar)
+for i in range(5):
+    driver.get(f'https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-1{str(i)}-decembrie-ora-13'
+               f'-00-2/')
+    dictionar[f'1{str(i)}.12.2021'] = []
 
+    if i == 0:  # First page
+        try:
+            button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div["
+                                                                                               "6]/div/div[3]/button["
+                                                                                               "1]")))
+            button.click()
+            time.sleep(1)
+            button2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div["
+                                                                                                "1]/div/a")))
+            button2.click()
+            time.sleep(1)
+        except NoSuchElementException as e:
+            print("Element not found: ", e)
 
+        table = driver.find_element(by=By.XPATH, value='/html/body/div[3]/div/div[1]/main/article/div/div/table['
+                                                       '1]/tbody')
+        table_list = table.text.split('\n')
+        del table_list[0]
+        for row_index, row_value in enumerate(table_list):
+            print(row_value)
+            if row_index < 42:
+                dictionar['Nr. crt.'].append(row_index + 1)
+                row_value_split = row_value.split(' ')
+                number = row_value_split[-2]
+                del row_value_split[0]
+                del row_value_split[-3:]
+                judet = str(row_value_split[0])
+                dictionar['Judet'].append(judet)
+                dictionar[f'1{str(i)}.12.2021'].append(number)
+            if row_index == 42:
+                dictionar['Nr. crt.'].append(43)
+                dictionar['Judet'].append('Din strainatate**')
+                row_value_split = row_value.split(' ')
+                dictionar[f'1{str(i)}.12.2021'].append(row_value_split[-3])
+            if row_index == 46:
+                dictionar['Nr. crt.'].append(44)
+                dictionar['Judet'].append('TOTAL')
+                row_value_split = row_value.split(' ')
+                dictionar[f'1{str(i)}.12.2021'].append(row_value_split[-3])
 
+    else:       # Next pages
+        table = driver.find_element(by=By.XPATH, value='/html/body/div[3]/div/div[1]/main/article/div/div/table['
+                                                       '1]/tbody')
+        table_list = table.text.split('\n')
+        del table_list[0]
+        for row_index, row_value in enumerate(table_list):
+            print(row_value)
+            if row_index < 42:
+                row_value_split = row_value.split(' ')
+                number = row_value_split[-2]
+                dictionar[f'1{str(i)}.12.2021'].append(number)
+            if row_index == 42:
+                row_value_split = row_value.split(' ')
+                if i == 3:
+                    dictionar[f'1{str(i)}.12.2021'].append(row_value_split[-1])
+                else:
+                    dictionar[f'1{str(i)}.12.2021'].append(row_value_split[-3])
+            if row_index == 46:
+                row_value_split = row_value.split(' ')
+                if i == 3:
+                    dictionar[f'1{str(i)}.12.2021'].append(row_value_split[-1])
+                else:
+                    dictionar[f'1{str(i)}.12.2021'].append(row_value_split[-3])
 
-
-
-
-
-
+df = pd.DataFrame(dictionar)
+df.to_csv('date_covid')
 
 
